c3016d963470771196f488bcdf5e9abbcf9fbd4c
Eric Masip Vilalta <ericmasipvilalta@gmail.com>
2024-11-12 21:17:14 +0100

    create and import navbar

---
+++ Kanban-Board/src/components/NavBar.jsx
@@ -0,0 +1,15 @@
+import './NavBar.css';
+
+function NavBar() {
+    return (
+        <nav>
+            <ul className='links'>
+                <li>Home</li>
+                <li>About</li>
+                <li>Contact</li>
+            </ul>
+        </nav>
+    )
+};
+
+export default NavBar;
