b30a0be440655c02859122bc889ea63d67587eac
Eric Masip Vilalta <ericmasipvilalta@gmail.com>
2024-11-13 21:09:18 +0100

    add footer and root styles

---
+++ Kanban-Board/src/components/Footer.jsx
@@ -0,0 +1,13 @@
+import './Footer.css'
+
+function Footer () {
+    return (
+        <ul className='footer-container'>
+            <li>KANBAN BOARD</li>
+            <li>By: Tomás & Èric</li>
+            <li href="https://github.com/Violinistapirata/Kanban-Board">GitHub</li>
+        </ul>
+    )
+}
+
+export default Footer;
