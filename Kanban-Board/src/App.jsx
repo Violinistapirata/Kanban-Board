6dcc24732afbb8e09fe9d72467a80349b7d47002
Tomás Martín <tmartindsgn@gmail.com>
2024-11-13 21:31:58 +0100

    sidebar added

--- Kanban-Board/src/App.jsx
+++ Kanban-Board/src/App.jsx
@@ -1,11 +1,13 @@
 import './App.css';
 import NavBar from './components/NavBar'
 import Footer from "./components/Footer";
+import SideBar from "./components/SideBar";
 
 function App() {
   return (
     <>
      <NavBar />
+     <SideBar/>
      <Footer />
     </>
   )
---
+++ Kanban-Board/src/components/SideBar.jsx
@@ -0,0 +1,13 @@
+import './SideBar.css'
+
+function SideBar () {
+    return (
+        <ul className='SideBar-container'>
+            <li>MY BOARDS</li>
+            <li>ARCHIVE BOARDS</li>
+           
+        </ul>
+    )
+}
+
+export default SideBar;
