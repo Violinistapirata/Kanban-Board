5bc7fa2520692e75e4a325ba3812136640c607b3
Eric Masip Vilalta <ericmasipvilalta@gmail.com>
2024-11-16 14:53:56 +0100

    display task titles in CurrentBoard

--- Kanban-Board/src/components/CurrentBoard.jsx
+++ Kanban-Board/src/components/CurrentBoard.jsx
@@ -1,8 +1,22 @@
+{/*  <li key={task.title}>
+     {task.title}
+ </li>
+ <li key={task.description}></li>
+ <li key={task.assignee}></li>
+ <li key={task.status}></li>
+ <li key={task.priority}></li>
+ <li key={task.dreateDate}></li>
+ <li key={task.dueDate}></li> */}
 import './CurrentBoard.css';
+// import { useParams } from 'react-router-dom';
 
 
 function CurrentBoard(props) {
     const taskList = props.taskList;
+    // const params = useParams();
+    console.log(taskList);
+
+
     const itemsId = taskList.map((item) => {
         const itemId = item.id
         // console.log(item.id);
@@ -14,9 +28,12 @@ function CurrentBoard(props) {
     // const taskList = 
     return (
         <>
-            <section className='current-board-container'>
-               <li>{itemsId[0]}</li>
-            </section>
+            <ul>{taskList.map((task) => (
+                <li key={task.id}>
+                    <h2>{task.title}</h2>
+                </li>
+                ))}
+            </ul>
         </>
     )
 };
