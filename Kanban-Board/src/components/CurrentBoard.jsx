92603f3635708f56dddf96c32357a23420cf48a1
Eric Masip Vilalta <ericmasipvilalta@gmail.com>
2024-11-19 20:49:56 +0100

    added delete button. Working in task deletion functionality.

--- Kanban-Board/src/components/CurrentBoard.jsx
+++ Kanban-Board/src/components/CurrentBoard.jsx
@@ -1,33 +1,34 @@
-{/*  <li key={task.title}>
-     {task.title}
- </li>
- <li key={task.description}></li>
- <li key={task.assignee}></li>
- <li key={task.status}></li>
- <li key={task.priority}></li>
- <li key={task.dreateDate}></li>
- <li key={task.dueDate}></li> */}
 import './CurrentBoard.css';
+import { useState } from 'react';
 // import { useParams } from 'react-router-dom';
 
-
 function CurrentBoard({taskList}) {
-
+    const [updateTaskList, setUpdateTaskList] = useState(taskList)
     // const params = useParams();
     return (
             <>
-                {taskList.map((task) => (
-                    <ul key={task.id}>
-                        <li >
-                            <h2>{task.title}</h2>
-                        </li>
-                        <li>{task.description}</li>
-                        <li>{task.assignee}</li>
-                        <li>{task.status} {task.status==="Done"? "✅" : task.status==="In Progress"? "✍️" : "❌" }</li>
-                        <li>{task.priority}</li>
-                        <li>{task.createdDate}</li>
-                        <li>{task.dueDate}</li>
-                    </ul>
+                {updateTaskList.map((task) => (
+                    <>
+                        <ul key={task.id}>
+                            <li >
+                                <h2>{task.title}</h2>
+                            </li>
+                            <li>{task.description}</li>
+                            <li>{task.assignee}</li>
+                            <li>{task.status} {task.status==="Done"? "✅" : task.status==="In Progress"? "✍️" : "❌" }</li>
+                            <li>{task.priority}</li>
+                            <li>{task.createdDate}</li>
+                            <li>{task.dueDate}</li>
+                        </ul>
+                        <button onClick={()=> {
+                            const taskIndex = taskList.indexOf(task);
+                            const newTaskList = taskList.splice(taskIndex, 1);
+                            console.log(taskList);
+                            setUpdateTaskList(newTaskList);
+                            }}>
+                            Delete Task
+                        </button>
+                    </>
                     ))}      
             </>
     )
