29f0369a32200fd0abeaffff0d57fc0ee9151a9f
Eric Masip Vilalta <ericmasipvilalta@gmail.com>
2024-11-21 16:47:23 +0100

    Iteration 4 solved. Passed list element and currentTaskList state to the List.jsx component.

--- Kanban-Board/src/components/CurrentBoard.jsx
+++ Kanban-Board/src/components/CurrentBoard.jsx
@@ -1,37 +1,19 @@
+// CSS
 import './CurrentBoard.css';
-import { useState } from 'react';
+
+// COMPONENTS
+import List from './List';
+
+// HOOKS
+// import { useState } from 'react';
 // import { useParams } from 'react-router-dom';
 
 function CurrentBoard({taskList}) {
-    const [currentTaskList, setCurrentTaskList] = useState(taskList)
+   
     // const params = useParams();
     return (
             <>
-                {currentTaskList.map((task) => (
-                    <>
-                        <ul key={task.id}>
-                            <li>
-                                <h2>{task.title}</h2>
-                            </li>
-                            <li>{task.description}</li>
-                            <li>{task.assignee}</li>
-                            <li>{task.status} {task.status==="Done"? "✅" : task.status==="In Progress"? "✍️" : "❌" }</li>
-                            <li>{task.priority}</li>
-                            <li>{task.createdDate}</li>
-                            <li>{task.dueDate}</li>
-                        </ul>
-                        <button onClick={()=> {
-                            const taskListCopy = [...currentTaskList]
-                            const taskId = task.id;
-                            const newTaskList = taskListCopy.filter((task)=>task.id !== taskId);
-                            // console.log(taskList);
-                            console.log(newTaskList);
-                            setCurrentTaskList(newTaskList);
-                            }}>
-                            Delete Task
-                        </button>
-                    </>
-                    ))}      
+                <List taskList={taskList} />
             </>
     )
 };
