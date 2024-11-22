c3ca7315e582184d6fb186a20b5d1e17d0eb30e3
Eric Masip Vilalta <ericmasipvilalta@gmail.com>
2024-11-22 19:31:24 +0100

    fixed the displaying of the new task details page

--- Kanban-Board/src/Pages/TaskDetailsPage.jsx
+++ Kanban-Board/src/Pages/TaskDetailsPage.jsx
@@ -5,7 +5,7 @@ import { useParams } from "react-router-dom";
 
 function TaskDetailsPage ({taskList}) {
     const { taskId } = useParams();    
-    const selectedTask = taskList.find((task) => task.id === taskId);
+    const selectedTask = taskList.find((task) => task.id == taskId);
     const {title, description, assignee, status, priority, createdDate, dueDate} = selectedTask;
     
     return (
