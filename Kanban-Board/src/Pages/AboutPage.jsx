31affd3e6966c54e09e11ab1577560542ddfd9f3
Tomás Martín <tmartindsgn@gmail.com>
2024-12-06 19:06:04 +0100

    styles abaout nearly done

--- Kanban-Board/src/Pages/AboutPage.jsx
+++ Kanban-Board/src/Pages/AboutPage.jsx
@@ -1,69 +1,63 @@
+import "./AboutPage.css";
+
 function AboutPage() {
   return (
     <section id="about">
-      <h1>ABOUT</h1>
+      <div className="about-container">
+        <h1>ABOUT</h1>
 
-      <img src="../src/images/developers.jpg" alt="developers" />
+        
+        <div id= "presentacion">
+        <h2>Presentación</h2>
+        <p>
+          Bienvenid@ a nuestro proyecto: <i>KANBAN BOARD</i>, tu app de gestión de tareas al más puro estilo Kanban. Somos Tomás Martín y Èric Masip, tus Fullstack Developers de confianza. En esta sección podrás conocer un poco más sobre nosotros y sobre este maravilloso proyecto.
+        </p>
+        </div>
+        <section id="developers">
+  <h2>LOS DEVELOPERS</h2>
 
-      <h2>Presentación</h2>
+  <div className="profiles-container">
+    <article id="eric-profile">
+      <h3>Èric Masip Vilalta - El Violinista Pirata</h3>
+      <img src="../src/images/ERIC.PNG" alt="Èric Masip Vilalta - El Violinista Pirata" />
       <p>
-        Bienvenid@ a nuestro proyecto: <i>KANBAN BOARD</i> tu app de gestión de
-        tareas al más puro estilo Kanban! Somos Tomás Martín y Èric Masip, tus
-        Fullstack Developers de confianza. En esta sección podrás conocer un
-        poco más sobre nosotros y sobre este maravilloso proyecto.
+        Empecé en el mundo de la música desde los 3 años. He estudiado y ejercido como violinista durante más de dos décadas, desarrollando habilidades como la{" "}
+        <b>resiliencia, la atención al detalle y el trabajo en equipo</b>. Después trabajé durante 6 años como profesor de música en la ESO y bachillerato, experiencia que fortaleció mis capacidades de{" "}
+        <b>comunicación, liderazgo y resolución de conflictos</b>.
       </p>
-      <section>
-        <h2>LOS DEVELOPERS</h2>
+      <p>
+        Actualmente, he encontrado mi pasión en el desarrollo web. Gracias al bootcamp de Ironhack, me convertí en{" "}
+        <b>Fullstack MERN Developer</b>, adquiriendo conocimientos en tecnologías como{" "}
+        <b>HTML, CSS, JavaScript, Node.js, React, Git/GitHub, OOP, TDD y MongoDB</b>. Mi objetivo es continuar aprendiendo, descubriendo y creando soluciones de alta calidad. En lo personal, me apasionan los juegos de mesa 🎲, el anime 👘👒, los bailes latinos 💃 y la repostería casera 🍪.
+      </p>
+    </article>
 
-        <article id="eric">
-          <h3>Èric Masip Vilalta - El Violinista Pirata</h3>
-          <p>
-            Empecé en el mundo de la música desde los 3 años. He estudiado y
-            ejercido como violinista durante más de 2 décadas y eso me ha hecho
-            desarrollar habilidades como la{" "}
-            <b>resiliencia, la atención al detalle y el trabajo en equipo.</b>{" "}
-            Después estuve otros 6 años trabajando en el sector educativo como
-            profesor de música en la ESO y bachillerato. Esta intensa pero
-            edificante experiencia me ayudó a desarrollar capacidades en{" "}
-            <b>comunicación, liderazgo y resolución de conflictos</b>
-            entre otras. Ahora he encontrado mi pasión en el desarrollo web y a
-            través de Ironhack he podido convertirme en{" "}
-            <b>Fullstack MERN Developer</b> y adquirir conocimientos técnicos en
-            el manejo de{" "}
-            <b>
-              HTML, CSS, JavaScript, Nodejs, React, Git/GitHub, OOP, TDD y
-              MongoDB,
-            </b>{" "}
-            entre otros. Mi objetivo es seguir aprendiendo, descubriendo y
-            creando en este fascinante mundo de la programación y poder ofrecer
-            soluciones de alta calidad para cualquier reto que pueda encontrarme
-            como desarrollador (Como buen gamer que soy no me achanto ante
-            ningún desafío y doy siempre el 100% para lograr mis objetivos 🏆).
-            En lo personal me considero una persona tranquila de trato agradable
-            y de múltiples y diversas aficiones entre las cuales destacan los
-            juegos de mesa🎲, el anime👘👒, los bailes latinos💃 y también la
-            repostería casera así que si te preguntabas de donde salían tantas
-            cookies ahora ya lo sabes 😜🍪.
-          </p>
-        </article>
+    <article id="tomas-profile">
+      <h3>Tomás Martín</h3>
+      <img src="../src/images/TOMAS.PNG" alt="Tomás Martín" />
+      <p>
+        ¡Hola! Mi nombre es Tomás Martín y me apasiona el diseño, la animación y, más recientemente, el desarrollo web.
+        Estudié <b>Diseño Gráfico y Multimedia</b> en la Universidad de Valencia, donde aprendí los fundamentos de composición, tipografía, fotografía y dibujo técnico, además de mi primer contacto con <b>HTML y CSS</b>.
+      </p>
+      <p>
+        Luego me especialicé en <b>Animación</b> en la Universidad de Newcastle, donde adquirí conocimientos sobre animación de personajes y gráficos. Posteriormente, trabajé como freelance mientras cursaba un postgrado en animación 3D, desarrollando habilidades avanzadas con <b>Cinema 4D (C4D)</b>.
+        Desde hace más de 11 años, he trabajado como <b>Motion Grapher</b> en Mediapro, participando en proyectos para televisión que exigen creatividad, trabajo en equipo y cumplir con estrictos deadlines. Ahora, gracias al bootcamp de Ironhack, adquirí conocimientos en <b>HTML, CSS, JavaScript, Node.js, React, Git/GitHub, OOP, TDD y MongoDB</b>. Estoy comprometido a seguir creciendo y enfrentando nuevos retos en el desarrollo web.
+      </p>
+    </article>
+  </div>
+</section>
 
-        <article id="eric">
-          <h3>Tomás Martín</h3>
-          <p>{/*DESCRIPCIÓN AQUÍ */}</p>
-        </article>
-      </section>
-      <section>
-        <h2>EL PROYECTO - KANBAN BOARD</h2>
-        <h3>El origen</h3>
-        <p></p>
-        <h3>Organización</h3>
-        <p></p>
-        <h3>Desafíos</h3>
-        <p></p>
-        <h3>Coming soon...</h3>
-        <p></p>
-      </section>
-      <div className="github-links-container">
+        <section id="project">
+          <h2>EL PROYECTO - KANBAN BOARD</h2>
+          <h3>El origen</h3>
+          <p></p>
+          <h3>Organización</h3>
+          <p></p>
+          <h3>Desafíos</h3>
+          <p></p>
+          <h3>Coming soon...</h3>
+          <p></p>
+        </section>
       </div>
     </section>
   );
