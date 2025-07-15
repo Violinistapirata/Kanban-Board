0495d11a646955055a48a42ed26cfb89ee4bf606
Eric Masip Vilalta <ericmasipvilalta@gmail.com>
2025-07-16 01:33:43 +0200

    refactored gitignore and added a target _blank property in footer anchor tag

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -6,7 +6,7 @@ function Footer () {
         <ul className='footer-container'>
             <li>KANBAN BOARD</li>
             <li>By: Tomás & Èric</li>
-            <li><a href="https://github.com/Violinistapirata/Kanban-Board">GitHub</a></li>
+            <li><a target='_blank' href="https://github.com/Violinistapirata/Kanban-Board">GitHub</a></li>
         </ul>
     )
 }
