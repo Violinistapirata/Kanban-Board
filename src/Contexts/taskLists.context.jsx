100c79d67c1ac6cacbe486323920adae50eff052
Eric Masip Vilalta <ericmasipvilalta@gmail.com>
2025-07-22 14:48:16 +0200

    adjusted layout and responsiveness of tasks and board container

--- src/Contexts/taskLists.context.jsx
+++ src/Contexts/taskLists.context.jsx
@@ -8,19 +8,40 @@ import { createContext, useState, useEffect } from "react";
 const TaskListContext = createContext();
 
 function TaskListProviderWrapper(props) {
-  const [boardsArray, setBoardsArray] = useState([]);
+  const [state, setState] = useState({
+    data: null,
+    loading: true,
+    error: null,
+  });
 
-  // Initialize boardsArray with KanbanJson data
+  const [boardsArray, setBoardsArray] = useState(state.data || []); // Initialize boardsArray with an empty array or state data
 
+  // Load data from localStorage or KanbanJson when the component mounts
+  // If localStorage has data, use it; otherwise, use KanbanJson and save it to localStorage
   useEffect(() => {
+    const loadData = async () => {
+      try {
     const storedBoards = localStorage.getItem("boardsArray");
     if (storedBoards) {
-      setBoardsArray(JSON.parse(storedBoards));
+      setState({data: JSON.parse(storedBoards), loading: false, error: null});
     } else {
-      setBoardsArray(KanbanJson);
+    setState({data: JSON.parse(KanbanJson), loading: false, error: null});
     localStorage.setItem("boardsArray", JSON.stringify(KanbanJson));
     }
+    } catch (err) {
+        setState({ data: null, loading: false, error: err.message });
+      }
+    }
+
+    loadData();
   }, []);
+
+  // Update boardsArray when state.data changes
+  useEffect(() => {
+      if (state.data) {
+        setBoardsArray(state.data);
+      }
+    }, [state.data]);
       /* { id: "1", name: "board 1", taskList: KanbanJson },
       {
         id: "2",
