cc86b883e45eba7a7de404706b657bd43573d17d
Eric Masip Vilalta <ericmasipvilalta@gmail.com>
2025-07-29 23:22:11 +0200

    Added functionality to edit the name of a board

--- src/Pages/DashboardPage.jsx
+++ src/Pages/DashboardPage.jsx
@@ -22,42 +22,88 @@ import "./DashboardPage.css";
 
 function DashboardPage() {
   console.log("The DashboardPage component has rendered");
-  const { boardsArray } = useContext(TaskListContext);
+  const { boardsArray, setBoardsArray } = useContext(TaskListContext);
 
   // State for the Add-board form
   const [showForm, setShowForm] = useState(false);
+  const [showEditNameInput, setShowEditNameInput] = useState(false);
+  const [boardToEdit, setBoardToEdit] = useState(null)
+  const [boardName, setBoardName] = useState(boardToEdit && boardToEdit.name || "" );
   const [showConfirmationPrompt, setShowConfirmationPrompt] = useState(false);
   const [boardToDelete, setBoardToDelete] = useState(null);
- 
+
+  console.log("THIS IS THE BOARD TO EDIT:", boardToEdit);
+  
+  function handleEditBoardName(e) {
+    e.preventDefault();
+
+    boardToEdit.name = boardName;
+    setBoardsArray([...boardsArray])
+    setBoardName("");
+    setBoardToEdit(null);
+    setShowEditNameInput(false);
+  }
+
+  function handleInput(e) {
+    const value = e.target.value;
+    setBoardName(value);
+  }
+
   function handleShowConfirmationPrompt(boardId) {
     console.log("THIS IS THE BOARD ID:", boardId);
 
     setShowConfirmationPrompt(true);
     const selectedBoard = boardsArray.find((board) => board.id === boardId);
     console.log("THIS IS THE SELECTED BOARD:", selectedBoard);
-    
+
     setBoardToDelete(selectedBoard);
   }
 
   return (
     <div className="boards-container">
       <div className="boards-sub-container">
-        {showConfirmationPrompt && <ConfirmationPrompt boardToDelete={boardToDelete} setBoardToDelete={setBoardToDelete} setShowConfirmationPrompt={setShowConfirmationPrompt}/>}
+        {showConfirmationPrompt && (
+          <ConfirmationPrompt
+            boardToDelete={boardToDelete}
+            setBoardToDelete={setBoardToDelete}
+            setShowConfirmationPrompt={setShowConfirmationPrompt}
+          />
+        )}
         <h1>MY BOARDS</h1>
         <div className="dashboard__boards-scroll">
           {boardsArray.map((board) => {
             return (
               <article key={board.id} className="board-card">
-                <Link to={`/current-board/${board.id}`}>
-                  <h2>{board.name}</h2>
-                </Link>
+                {showEditNameInput && boardToEdit && boardToEdit.id === board.id ? (
+                  <form onSubmit={handleEditBoardName}>
+                    <input
+                      value={boardName}
+                      placeholder={board.name}
+                      onChange={handleInput}
+                    ></input>
+                  </form>
+                ) : (
+                  <Link to={`/current-board/${board.id}`}>
+                    <h2>{board.name}</h2>
+                  </Link>
+                  
+                )}
+
+                <button
+                  className="edit-button"
+                  onClick={() => {
+                    setShowEditNameInput(true)
+                    setBoardToEdit(board)
+                    }}
+                >
+                  <img src="/images/edit-pencil.svg"></img>
+                </button>
                 <DeleteButton
                   handleF={() => handleShowConfirmationPrompt(board.id)}
                 />
               </article>
             );
           })}
-          {/* {boardsArray.map(board => <CurrentBoard key={board.id} taskList={board.taskList} /> )} */}
         </div>
         {showForm && <CreateBoardForm setShowForm={setShowForm} />}
         <NewBoardButton setShowForm={setShowForm} />
