d9a2a4af84a6812a8fc8ea79ef4df9a071f4c8a7
Eric Masip Vilalta <ericmasipvilalta@gmail.com>
2025-08-01 19:46:37 +0200

    Added menu with links to each section and toggle menu function

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,25 +1,74 @@
+//LINKS
+import { Link, NavLink } from "react-router-dom";
+
 //HOOKS
-import { NavLink } from "react-router-dom"; 
+import { useState } from "react";
 
 //STYLES
 import "./NavBar.css";
 
 function NavBar() {
   console.log("The NavBar component has rendered");
+
+  const [menuIsOpen, setMenuIsOpen] = useState(false);
+
+  function toggleMenu() {
+    setMenuIsOpen(!menuIsOpen);
+  }
+
   return (
     <nav className="navbar-container">
-        <NavLink to="/" className="logo">
-            <img src="/images/LOGO.PNG" alt="Logo" />
-        </NavLink>
+      <NavLink to="/" className="logo">
+        <img src="/images/LOGO.PNG" alt="Logo" />
+      </NavLink>
 
-        <button type="button" className="menu-button">
-          <img src="/images/menu-icon.svg" alt="menu button" />
-        </button>
+      <button type="button" className="menu-button" onClick={toggleMenu}>
+        <img src="/images/menu-icon.svg" alt="menu button" />
+      </button>
+
+      {menuIsOpen && (
+        <ul className="menu-container">
+          <li>
+            <Link
+              to="/" 
+              onClick={toggleMenu}
+            >
+              MY BOARDS
+            </Link>
+          </li>
+          <li>
+            <Link
+              to="/archived" 
+              onClick={toggleMenu} 
+            >
+              ARCHIVED BOARDS
+            </Link>
+          </li>
+          <li>
+            <Link
+              to="/about" 
+              onClick={toggleMenu}
+            >
+              ABOUT
+            </Link>
+          </li>
+          <li>
+            <Link
+              to="/contact" 
+              onClick={toggleMenu}
+            >
+              CONTACT
+            </Link>
+          </li>
+        </ul>
+      )}
       <ul className="navbar-tabs-container">
         <li>
           <NavLink
             to="/"
-            className={({ isActive }) => (isActive ? "navbar-button active" : "navbar-button")}
+            className={({ isActive }) =>
+              isActive ? "navbar-button active" : "navbar-button"
+            }
           >
             Home
           </NavLink>
@@ -27,7 +76,9 @@ function NavBar() {
         <li>
           <NavLink
             to="/about"
-            className={({ isActive }) => (isActive ? "navbar-button active" : "navbar-button")}
+            className={({ isActive }) =>
+              isActive ? "navbar-button active" : "navbar-button"
+            }
           >
             About
           </NavLink>
@@ -35,7 +86,9 @@ function NavBar() {
         <li>
           <NavLink
             to="/contact"
-            className={({ isActive }) => (isActive ? "navbar-button active" : "navbar-button")}
+            className={({ isActive }) =>
+              isActive ? "navbar-button active" : "navbar-button"
+            }
           >
             Contact
           </NavLink>
